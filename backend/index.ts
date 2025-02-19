a4eae6999d0793271951525f1f4b7228ed4cd6dc
devarsh281 <devarsh.doshi@technostacks.com>
2025-02-19 14:02:18 +0530

    port updation

--- backend/index.ts
+++ backend/index.ts
@@ -83,9 +83,9 @@ app.get("/messages", async (_req: Request, res: Response) => {
   res.json(allMessages);
 });
 
-app.get('/hello', (req, res) => {
-  res.json({ message: 'Hello from backend' });
-});
+// app.get('/hello', (req, res) => {
+//   res.json({ message: 'Hello from backend' });
+// });
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
