4718a5712e4c8e2093be3dcccd35bf2c24ed0571
devarsh281 <devarsh.doshi@technostacks.com>
2025-02-19 12:52:32 +0530

    cors updation

--- backend/index.ts
+++ backend/index.ts
@@ -10,7 +10,7 @@ app.use(cors({
 }));
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 const generateResponse = (input: string): string => {
   const emojisInInput = Array.from(input).filter((char) =>
